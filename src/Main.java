53578fa5e3c161b31fc62eef2c1fcfe0cf023aed
Diego <diegodlx@gmail.come.com>
2023-06-05 23:29:56 -0300

    Implementado classe Ave Diego Loyola

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import animal.Ave;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+
+        Ave teste = new Ave("teste", "teste2", 18);
+
+        teste.comer();
     }
 }
---
+++ src/animal/Ave.java
@@ -0,0 +1,34 @@
+package animal;
+
+public class Ave extends Animal{
+    public Ave(String nome, String tipo, int idade) {
+        super(nome, tipo, idade);
+    }
+
+    private String raca;
+    private String porte;
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public void voar(){
+        System.out.println("Estou voando pela casa");
+    }
+
+    public void comer(){
+        System.out.println("Estou comendo semente de girassol");
+    }
+}
