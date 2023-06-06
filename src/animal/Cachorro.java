dae363bfd67b977d4b7b73c9813336ad06a2cbcd
cleysonseveriano <cleyson.tab@gmail.com>
2023-06-06 20:29:01 -0300

    Adicionando métodos getters and setters na classe Cachorro

--- src/animal/Cachorro.java
+++ src/animal/Cachorro.java
@@ -4,6 +4,22 @@ public class Cachorro extends Animal {
     private String raca;
     private String porte;
 
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
     public Cachorro(String nome, String tipo, int idade) {
         super(nome, tipo, idade);
     }
@@ -14,8 +30,6 @@ public class Cachorro extends Animal {
 
     public void morder(){
         System.out.println("NHEC NHEC NHEC");
-
-
     }
 
     public void correr(){
